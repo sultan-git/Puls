521998fcf0168d1bc8b5661a2840af9530ba077a
Sultan <sultanu78@mail.ru>
2020-12-26 20:47:50 +0500

    Установил слайдер Slick

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+// $(document).ready(function () {
+// 	$('.carousel__inner').slick({
+// 		speed: 1200,
+// 		// adaptiveHeight: true,
+// 		prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+// 		nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+// 		responsive: [{
+// 			breakpoint: 767,
+// 			settings: {
+// 				arrows: false,
+// 				dots: true
+// 			}
+// 		}]
+
+// 	});
+// });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    speed: 1200,
+    nav: false
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev'); 
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next'); 
+  });
