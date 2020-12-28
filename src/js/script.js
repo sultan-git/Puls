bf03e44b38e4b1472e0118443eb2410621fc851c
Sultan <sultanu78@mail.ru>
2020-12-28 13:27:34 +0500

    Добавил слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -15,20 +15,54 @@
 // 	});
 // });
 
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    speed: 1200,
-    nav: false
-  });
+// const slider = tns({
+//   container: '.carousel__inner',
+//   items: 1,
+//   slideBy: 'page',
+//   autoplay: false,
+//   controls: false,
+//   speed: 1200,
+//   nav: false,
+//   responsive: {
+//     575: {
+//       nav: true,
+//       navPosition: 'bottom'
+//     }
+//   }
+// });
 
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev'); 
-  });
+// document.querySelector('.prev').addEventListener('click', function () {
+//   slider.goTo('prev');
+// });
 
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next'); 
+// document.querySelector('.next').addEventListener('click', function () {
+//   slider.goTo('next');
+// });
+
+
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    autoPlay: true,
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          dots: true,
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ]
   });
+});
